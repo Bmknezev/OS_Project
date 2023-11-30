953ec7e782b28022127d96954c7e5eacafa9ff6e
braydon <bmknezev@lakeheadu.ca>
2023-11-30 12:16:08 -0500

    changed modify command

--- Command Functions/Text Files/modifyTxt.cpp
+++ Command Functions/Text Files/modifyTxt.cpp
@@ -12,17 +12,29 @@ void modifyTxt(string fileName) {
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << "Enter the data to append: ";
 
-    getline(cin, newData);
-
     string temp;
     ostringstream readFromFile;
     ifstream readFile(fileName);
     for (int i = 0; i < numberOfCharacters; i++) {
+        if(readFile.eof())
+            break;
         readFromFile << char(readFile.get() );
     }
-    readFromFile << newData;
-    readFile >> temp;
-    readFromFile << temp;
+    while (getline( std::cin, newData ))
+    {
+        // Find ^X in s
+        auto n = newData.find( '\x18' );
+        if (n != newData.npos) newData = newData.substr( 0, n );
+
+        readFromFile << newData << endl;
+
+        // If ^C was found in s, we're done reading input from user
+        if (n != newData.npos) break;
+    }
+    while(!readFile.eof())
+        readFromFile << char(readFile.get());
+
+    //readFromFile << temp;
     ofstream out(fileName);
     temp = readFromFile.str();
     out << temp;
